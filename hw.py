cbb7160bfc3f955588e3a9a481898328587d0a9b
Tim Dodge <tim.dodge64@gmail.com>
2023-01-18 11:49:26 -0700

    stuff

--- hw.py
+++ hw.py
@@ -1,5 +1,7 @@
 import numpy as np
+from sys import argv
 import matplotlib.pyplot as plt
+from multiprocessing import Pool
 
 
 def generate_random_lists(n: int):
@@ -11,55 +13,57 @@ def generate_random_lists(n: int):
     Returns:
         list of lists of random numbers
     """
-    np.random.seed(40)
+    np.random.seed(100)
     lists = [np.random.randint(n, size=(n)) for _ in range(n)]
     return lists
 
+def loop_logic(number: int, lists):
+    # create a list of successes for the given index
+    successes = []
+    for index_list in lists:
+        # find the max value in the list
+        max_ = max(index_list)
+        # start the max loop at -inf
+        if number == 0:
+            max_look = index_list[0]
+        else:
+            max_look = max(index_list[:number])
+        for j in range(len(index_list[number:])):
+            if index_list[j] > max_look:
+                # if the value at the index is the actual max
+                if index_list[j] == max_:
+                    successes.append(1)
+                break
+    # find the precent of total successes for the given index
+    return (number, len(successes))
 
-def create_stats():
-    np.random.seed(40)
+
+def create_stats(file = None):
+    np.random.seed(100)
     n = 1000
     # generate random lists
-    lists = generate_random_lists(n)
-    precent_success_for_given_index = []
+    if file:
+        with open(file) as f:
+            lists = [np.genfromtxt(f)]
+    else:
+        lists = generate_random_lists(n)
+    precent_success_for_given_index = np.zeros(n)
 
     # loop through all the indices
-    for i in range(n):
-        print(f"i = {i}...")
-        # create a list of successes for the given index
-        successes = []
-        for index_lis in lists:
-            # find the max value in the list
-            max_ = max(index_lis)
-            # start the max loop at -inf
-            max_look = -np.inf
-            for j in range(n):
-                # while the index is less than the loop index for cutoff of 
-                # look time
-                if i >= j:
-                    # if the value at the index is greater than the max look
-                    max_look = max(max_look, index_lis[j])
-                # after the look time
-                else:
-                    # if the value at the index is greater than the max look
-                    if index_lis[j] >= max_look:
-                        # print(f"index_lis[j] = {index_lis[j]} > max_look = {max_look}")
-                        # if the value at the index is the actual max
-                        if index_lis[j] == max_: 
-                            # print(f"index_lis[j] = {index_lis[j]} == max_ = {max_}")
-                            successes.append(1)
-                        break
+    list_of_index = [(i, lists) for i in range(n)]
+    with Pool() as pool:
+        for result in pool.starmap(loop_logic, list_of_index):
+            number, success = result
+            precent_success_for_given_index[number] = success
+        pool.close()
+    print(precent_success_for_given_index.shape)
 
-        # find the precent of total successes for the given index
-        precent_success_for_given_index.append(len(successes))
-    plt.bar([i for i in range(n)], precent_success_for_given_index)
+    plt.bar(np.arange(n), precent_success_for_given_index)
     plt.show()
 
 if __name__ == "__main__":
-    create_stats()
-        
-        
-        
-
-
+    if len(argv) == 2:
+        create_stats(argv[1])
+    else:
+        create_stats()
 
